a2d11673e02c2522e6f88ee7195429b9568b9dde
Wanderson Duarte Alves <wandersondrtlvs.new@gmail.com>
2023-02-03 00:24:05 -0300

    Created conection with mysql and any routes

---
+++ recursos_humanos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('recursos_humanos', views.recursos_humanos),
+    path('authorized', views.authorized),
+]
---
+++ recursos_humanos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def recursos_humanos(request):
+    # return HttpResponse('Hello World!')
+    return render(request, 'home/index.html', {'today': datetime.today()})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
